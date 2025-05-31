f4c9b5c51d3b27559406b33718df1ba347d74af1
Jake Faulkner <jakefaulkn@gmail.com>
2025-05-31 13:44:48 +1200

    remove junk

--- src/main.c
+++ src/main.c
@@ -1,52 +1,21 @@
 #include <bluetooth/bluetooth.h>
 #include <cwiid.h>
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-typedef struct {
-    float x;
-    float y;
-} pos2d;
 
 #define FOV_X 45.0
 #define FOV_Y 45.0
 #define CY 384.0
 #define CX 512.0
 
-void print_buttons(uint16_t buttons) {
-    printf("Buttons pressed:");
-    if (buttons & CWIID_BTN_LEFT)
-        printf(" LEFT");
-    if (buttons & CWIID_BTN_RIGHT)
-        printf(" RIGHT");
-    if (buttons & CWIID_BTN_UP)
-        printf(" UP");
-    if (buttons & CWIID_BTN_DOWN)
-        printf(" DOWN");
-    if (buttons & CWIID_BTN_A)
-        printf(" A");
-    if (buttons & CWIID_BTN_B)
-        printf(" B");
-    if (buttons & CWIID_BTN_1)
-        printf(" 1");
-    if (buttons & CWIID_BTN_2)
-        printf(" 2");
-    if (buttons & CWIID_BTN_MINUS)
-        printf(" MINUS");
-    if (buttons & CWIID_BTN_PLUS)
-        printf(" PLUS");
-    if (buttons & CWIID_BTN_HOME)
-        printf(" HOME");
-    printf("\n");
-}
+float screen_x = 0;
+float screen_y = 0;
+
+void print_buttons(uint16_t buttons) {}
 
 void ir_to_real_space(uint16_t px1, uint16_t py1, uint16_t px2, uint16_t py2,
                       float *screen_x, float *screen_y) {
-
-    float mid_y =
-        ((float)(py1 + py2)) / 2.0 +
-        30; // + 30 = manual offset since you are typically above the screen
+    float mid_y = ((float)(py1 + py2)) / 2.0;
     float mid_x = ((float)(px1 + px2)) / 2.0;
     float offset_y = (CY - mid_y) / 768;
     float offset_x = (CX - mid_x) / 1024;
@@ -55,8 +24,6 @@ void ir_to_real_space(uint16_t px1, uint16_t py1, uint16_t px2, uint16_t py2,
 }
 
 void print_ir_event(struct cwiid_ir_src srcs[]) {
-    float screen_x = 0;
-    float screen_y = 0;
     uint16_t px1 = 0;
     uint16_t px2 = 0;
     uint16_t py1 = 0;
@@ -83,7 +50,6 @@ void print_ir_event(struct cwiid_ir_src srcs[]) {
 
 void cwiid_callback(cwiid_wiimote_t *wiimote, int mesg_count,
                     union cwiid_mesg mesg_array[], struct timespec *timestamp) {
-    printf("Got a callback!\n");
     for (int i = 0; i < mesg_count; i++) {
         union cwiid_mesg msg = mesg_array[i];
         switch (msg.type) {
@@ -112,14 +78,11 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("Press buttons 1+2 on your Wiimote now...\n");
     wiimote = cwiid_open(&bdaddr, CWIID_FLAG_MESG_IFC);
-    printf("HUH!\n");
     if (!wiimote) {
         fprintf(stderr, "Unable to connect\n");
         return 1;
     }
-    printf("Connected.\n");
 
     if (cwiid_set_mesg_callback(wiimote, &cwiid_callback)) {
         fprintf(stderr, "Unable to set callback\n");
@@ -131,11 +94,6 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Unable to set report mode\n");
         cwiid_close(wiimote);
         return 1;
-    }
-
-    printf("Listening for button presses. Press Ctrl+C to exit.\n");
-    while (1) {
-        sleep(1);
     }
 
     cwiid_close(wiimote);
