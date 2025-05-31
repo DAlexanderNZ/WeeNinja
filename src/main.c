5f78330915e8396c17f498029811c506014a61c0
Jake Faulkner <jakefaulkn@gmail.com>
2025-05-31 22:00:47 +1200

    kalman filtered input values

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "application.h"
 #include "fruit.h"
+#include "input.h"
 #include "menu.h"
 #include "message.h"
 #include "model.h"
@@ -14,13 +15,11 @@
 #define FOV_X 45.0
 #define FOV_Y 45.0
 #define FLICK_THRESHOLD 3.0
-#define CY 384.0
-#define CX 512.0
 
 Vector2 screen = {320, 240};
 Vector2 screen_extents = {320, 240};
 Vector2 targetScreen = {320, 240};
-struct acc_cal wm_cal;
+
 int flicking = false;
 Vector2 flick_direction = {0.0, 0.0};
 Vector2 shot_start = {0.0, 0.0};
@@ -35,48 +34,6 @@ void print_buttons(uint16_t buttons) {
     }
 }
 
-void ir_to_real_space(uint16_t px1, uint16_t py1, uint16_t px2, uint16_t py2,
-                      Vector2 *output_screen_coords) {
-    float mid_y = ((float)(py1 + py2)) / 2.0;
-    float mid_x = ((float)(px1 + px2)) / 2.0;
-
-    float offset_y = -(CY - mid_y) / screen_extents.y;
-    float offset_x = (CX - mid_x) / screen_extents.x;
-
-    output_screen_coords->x =
-        screen_extents.x / 2 + offset_x * screen_extents.x;
-    output_screen_coords->y =
-        screen_extents.y / 2 + offset_y * screen_extents.y;
-}
-
-void handle_accel_event(struct cwiid_acc_mesg msg) {}
-
-void track_ir_event(struct cwiid_ir_src srcs[]) {
-    uint16_t px1 = 0;
-    uint16_t px2 = 0;
-    uint16_t py1 = 0;
-    uint16_t py2 = 0;
-    int blob_count = 0;
-
-    for (int i = 0; i < CWIID_IR_SRC_COUNT; i++) {
-        if (srcs[i].valid) {
-            if (blob_count == 0) {
-                px1 = srcs[i].pos[CWIID_X];
-                py1 = srcs[i].pos[CWIID_Y];
-                blob_count++;
-            } else if (blob_count == 1) {
-                px2 = srcs[i].pos[CWIID_X];
-                py2 = srcs[i].pos[CWIID_Y];
-                blob_count++;
-            } else {
-                break;
-            }
-        }
-    }
-    if (blob_count == 2) {
-        ir_to_real_space(px1, py1, px2, py2, &targetScreen);
-    }
-}
 void DrawSlicer(Camera camera, Vector2 at) {
     Ray ray = GetScreenToWorldRay(at, camera);
     // Project the ray direction onto the z = 0 plane from the ray position
@@ -86,116 +43,27 @@ void DrawSlicer(Camera camera, Vector2 at) {
     DrawSphere(OnZ0Plane, 0.1, (Color){0, 0, 255, 85});
 }
 
-void cwiid_callback(cwiid_wiimote_t *wiimote, int mesg_count,
-                    union cwiid_mesg mesg_array[], struct timespec *timestamp) {
-    for (int i = 0; i < mesg_count; i++) {
-        union cwiid_mesg msg = mesg_array[i];
-        switch (msg.type) {
-        case CWIID_MESG_BTN:
-            print_buttons(msg.btn_mesg.buttons);
-            break;
-        case CWIID_MESG_IR:
-            track_ir_event(msg.ir_mesg.src);
-            break;
-        case CWIID_MESG_ACC:
-            handle_accel_event(msg.acc_mesg);
-            break;
-        default:
-            break;
-        }
-    }
-}
-
 Vector2 Lerp2(Vector2 from, Vector2 to, float alpha) {
     return (Vector2){from.x + alpha * (to.x - from.x),
                      from.y + alpha * (to.y - from.y)};
 }
 
 int main(int argc, char **argv) {
-    cwiid_wiimote_t *wiimote;
+
     int use_wiimote = argc == 2 && !strncmp(argv[1], "YES", 3);
-    printf("Using wiimote: %d\n", use_wiimote);
     if (use_wiimote) {
-        bdaddr_t bdaddr = *BDADDR_ANY;
-
-        wiimote = cwiid_open(&bdaddr, CWIID_FLAG_MESG_IFC);
-        if (!wiimote) {
-            fprintf(stderr, "Unable to connect\n");
-            return 1; // Exit on failure to connect
-        }
-
-        if (cwiid_set_mesg_callback(wiimote, &cwiid_callback)) {
-            fprintf(stderr, "Unable to set callback\n");
-            cwiid_close(wiimote);
-            return 1;
-        }
-
-        if (cwiid_set_rpt_mode(wiimote,
-                               CWIID_RPT_BTN | CWIID_RPT_IR | CWIID_RPT_ACC)) {
-            fprintf(stderr, "Unable to set report mode\n");
-            cwiid_close(wiimote);
-            return 1;
-        }
-        cwiid_get_acc_cal(wiimote, CWIID_EXT_NONE, &wm_cal);
+        init_input();
     }
 
-    InitWindow(640, 480, "WeeNinja");
-
-    ToggleFullscreen();
-    screen_extents.x = (float)GetScreenWidth();
-    screen_extents.y = (float)GetScreenHeight();
-    Camera3D camera = {0};
-    camera.position = (Vector3){0.0f, 0.0f, 1.0f};
-    camera.target = (Vector3){0.0f, 0.0f, -1.0f};
-    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
-    camera.projection = CAMERA_PERSPECTIVE;
-    camera.fovy = 45.0f;
-
-    GameState state;
-    wn_state_init(&state);
-
-    int shouldQuit = 0;
-    screen = targetScreen;
-
-    float fruit_timer = 0.0f;
-    while (!WindowShouldClose() && !shouldQuit) {
-        PollInputEvents();
-
-        screen = Lerp2(screen, targetScreen, 0.7);
-
-        if (shooting) {
-            Ray ray = GetScreenToWorldRay(shot_start, camera);
-            wn_fruit_pick(&state, ray);
-        }
-
-        BeginDrawing();
-        BeginMode3D(camera);
-
-        ClearBackground(WHITE);
-
-        DrawSlicer(camera, screen);
-
-        fruit_timer += GetFrameTime();
-        if (fruit_timer > 1.0f) {
-            fruit_timer = 0.0f;
-            wn_spawnfruit(&state, rand() % _N_FRUIT);
-        }
-
-        wn_update(&state);
-        wn_drawfruit(&state);
-
-        EndMode3D();
-
-        /* shouldQuit = handleMsg(menu()); */
-
-        /* menu(); */
-        EndDrawing();
+    while (1) {
+        float position[3] = {0.0, 0.0, 0.0};
+        poll_position(position);
+        printf("Est position: (%f, %f, %f)", position[0], position[1],
+               position[2]);
     }
 
     if (use_wiimote) {
-        cwiid_close(wiimote);
+        free_input();
     }
-
-    CloseWindow();
     return 0;
 }
