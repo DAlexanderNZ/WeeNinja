3bb2de49fe87ce87778861ddd0efe36e754b8404
Daniel Alexander <5389473+DAlexanderNZ@users.noreply.github.com>
2025-06-29 15:37:20 +1200

    fix slicer not lining up with mouse in fullscreen

--- src/main.c
+++ src/main.c
@@ -41,7 +41,6 @@ int main(int argc, char **argv) {
     }
 
     InitWindow(640, 480, "WeeNinja");
-
     ToggleFullscreen();
     screen_width = GetScreenWidth();
     screen_height = GetScreenHeight();
@@ -196,6 +195,8 @@ int main(int argc, char **argv) {
             break;
         }
         }
+        // Set the Window size to the render size so the mouse pos lines up for the slicer
+        SetWindowSize(GetRenderWidth(),GetRenderHeight());
     }
 
     if (use_wiimote) {
